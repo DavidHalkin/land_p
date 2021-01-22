ad3d5454b69925fc92a0ea371a411ba4e778f4d1
David_Halkin <northfire@mail.ru>
2021-01-22 15:31:52 +0200

    compressed

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ gulp.task('css-libs', function() {
 gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src('app/sass/**/*.scss') // Берем источник
 		.pipe(mode.dev(sourcemaps.init()))
-		.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		.pipe(mode.dev(sourcemaps.write('')))
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
